7868c19412b9e0bf69bf900159005d20eb8cceda
David Ghedini <david@davidghedini.com>
2020-03-02 04:15:03 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ from sphinx.locale import _
 from sphinx_rtd_theme import __version__
 
 
-project = u'AcuGIS Suite'
+project = u'JRI Publisher'
 slug = re.sub(r'\W+', '-', project.lower())
 version = __version__
 release = __version__
